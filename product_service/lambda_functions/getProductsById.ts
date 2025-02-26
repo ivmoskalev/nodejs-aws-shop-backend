71e64dea248c97f3a27970bb4d3e64daf325fae1
Igor Moskalev <moskalev.igor.v@gmail.com>
2025-02-26 17:10:17 +0500

    feat: add api gateway and lambda function infrastructure implementation

---
+++ product_service/lambda_functions/getProductsById.ts
@@ -0,0 +1,74 @@
+import {
+    Context,
+    APIGatewayProxyHandler,
+    APIGatewayProxyResult,
+    APIGatewayEvent
+  } from 'aws-lambda';
+  import { IProduct } from './product.interface';
+
+  // Hardcoded mock data
+  const MOCK_PRODUCTS: IProduct[] = [
+    {
+      id: "1",
+      title: "Product 1",
+      description: "Product 1 description",
+      price: 89.99
+    },
+    {
+      id: "2",
+      title: "Product 2",
+      description: "Product 2 description",
+      price: 110.50
+    },
+    {
+      id: "3",
+      title: "Product 3",
+      description: "Product 3 description",
+      price: 249.99
+    }
+  ];
+
+  const defaultHeaders = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  };
+
+  export const handler: APIGatewayProxyHandler = async (
+    event: APIGatewayEvent,
+    context: Context
+  ): Promise<APIGatewayProxyResult> => {
+    try {
+      const productId = event.pathParameters?.id;
+
+      if (!productId) {
+        return {
+          statusCode: 400,
+          headers: defaultHeaders,
+          body: JSON.stringify({ message: 'Product ID is required' }),
+        };
+      }
+
+      const product = MOCK_PRODUCTS.find(p => p.id === productId);
+
+      if (!product) {
+        return {
+          statusCode: 404,
+          headers: defaultHeaders,
+          body: JSON.stringify({ message: `Product with ID ${productId} not found` }),
+        };
+      }
+
+      return {
+        statusCode: 200,
+        headers: defaultHeaders,
+        body: JSON.stringify(product),
+      };
+    } catch (error) {
+      console.error('Error processing request:', error);
+      return {
+        statusCode: 500,
+        headers: defaultHeaders,
+        body: JSON.stringify({ message: 'Internal server error' }),
+      };
+    }
+  };
--- product_service/lib/product_service-stack.ts
+++ product_service/lib/product_service-stack.ts
@@ -1,16 +1,70 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  Aws,
+  CfnOutput,
+  Duration,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+  Tags
+} from 'aws-cdk-lib';
 
-export class ProductServiceStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+import { Construct } from 'constructs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambda_nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
+
+export class ProductServiceStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const nodejsFunctionProps = {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      memorySize: 128,
+      timeout: Duration.seconds(100),
+      tracing: lambda.Tracing.ACTIVE,
+    };
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProductServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const getProductsListFunction = new lambda_nodejs.NodejsFunction(this, 'GetProductsListHandler', {
+      entry: './lambda_functions/getProductsList.ts',
+      handler: 'handler',
+      ...nodejsFunctionProps,
+    });
+
+    new CfnOutput(this, 'getProductsListFunction', {
+      description: 'Lambda function used to show list of products',
+      value: getProductsListFunction.functionName,
+    });
+
+    const getProductsByIdFunction = new lambda_nodejs.NodejsFunction(this, 'GetProductsByIdHandler', {
+      entry: './lambda_functions/getProductsById.ts',
+      handler: 'handler',
+      ...nodejsFunctionProps,
+    });
+
+    new CfnOutput(this, 'getProductsByIdFunction', {
+      description: 'Lambda function used to receive products by id',
+      value: getProductsByIdFunction.functionName,
+    });
+
+    const api = new apigateway.RestApi(this, 'products-api', {
+      deployOptions: {
+        stageName: 'dev',
+        tracingEnabled: true,
+      }
+    });
+
+    Tags.of(api).add('Name', `${Aws.STACK_NAME}-api`);
+    Tags.of(api).add('Stack', `${Aws.STACK_NAME}`);
+
+    new CfnOutput(this, 'ProductsApi', {
+      description: 'API Gateway endpoint URL',
+      value: api.url,
+    });
+
+    const products = api.root.addResource('products');
+    products.addMethod('GET', new apigateway.LambdaIntegration(getProductsListFunction));
+
+    const product = products.addResource('{id}');
+    product.addMethod('GET', new apigateway.LambdaIntegration(getProductsByIdFunction));
   }
 }
